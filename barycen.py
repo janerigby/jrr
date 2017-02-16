ed55a13cc8740eed726a3b8d4ba04faccbfa44d2
Jane Rigby <Jane.Rigby@gmail.com>
2017-02-16 09:58:51 -0800

    do barycentric correction

---
+++ barycen.py
@@ -0,0 +1,57 @@
+''' This is code to compute the barycentric velocity correction, as documented in jskycalc.
+Adapted from https://gist.github.com/StuartLittlefair/5aaf476c5d7b52d20aa9544cfaa936a1
+Eventually such a wrapper function will get written into astropy.  In the meantime, using this 
+stopgap.
+I have verified that this code gives the same answers as jskycalc, for a few positions,
+and a few observatories (Las Campanas and Keck), to within 0.01 km/s.
+jrigby, Feb 2017'''
+
+from astropy.time import Time
+from astropy import coordinates
+from astropy.coordinates import SkyCoord, solar_system, EarthLocation, ICRS
+from astropy import units as u
+
+def velcorr(mytime, skycoord, location=None):
+  """Barycentric velocity correction.
+  
+  Uses the ephemeris set with  ``astropy.coordinates.solar_system_ephemeris.set`` for corrections. 
+  For more information see `~astropy.coordinates.solar_system_ephemeris`.
+  
+  Parameters
+  ----------
+  mytime : `~astropy.time.Time`
+    The time of observation.
+  skycoord: `~astropy.coordinates.SkyCoord`
+    The sky location to calculate the correction for.
+  location: `~astropy.coordinates.EarthLocation`, optional
+    The location of the observatory to calculate the correction for.
+    If no location is given, the ``location`` attribute of the Time
+    object is used
+    
+  Returns
+  -------
+  vel_corr : `~astropy.units.Quantity`
+    The velocity correction to convert to Barycentric velocities. Should be added to the original
+    velocity.
+  """
+  
+  if location is None:
+    if mytime.location is None:
+        raise ValueError('An EarthLocation needs to be set or passed '
+                         'in to calculate bary- or heliocentric '
+                         'corrections')
+    location = mytime.location
+    
+  # ensure sky location is ICRS compatible
+  if not skycoord.is_transformable_to(ICRS()):
+    raise ValueError("Given skycoord is not transformable to the ICRS")
+  
+  ep, ev = solar_system.get_body_barycentric_posvel('earth', mytime) # ICRS position and velocity of Earth's geocenter
+  op, ov = location.get_gcrs_posvel(mytime) # GCRS position and velocity of observatory
+  # ICRS and GCRS are axes-aligned. Can add the velocities
+  velocity = ev + ov # relies on PR5434 being merged
+  
+  # get unit ICRS vector in direction of SkyCoord
+  sc_cartesian = skycoord.represent_as(coordinates.UnitSphericalRepresentation).represent_as(coordinates.CartesianRepresentation)
+  return sc_cartesian.dot(velocity).to(u.km/u.s) # similarly requires PR5434
+
