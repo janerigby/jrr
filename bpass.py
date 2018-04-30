feb3c6a9da7cd513ee0c9fa8b6dbb0f1a338c68e
Rigby <Jane.Rigby@gmail.com>
2018-04-30 16:05:54 -0400

    bpass spectrum import module works OK

--- bpass.py
+++ bpass.py
@@ -27,17 +27,18 @@ def load_spectra(filename) :  # Loads spectra for all ages.  1st col is waveleng
 def load_1spectrum(filename, age_to_find) : #Streamline version of above, just loads 1 age.  Faster.  Uses filename
     bpassdir = model_dir()
     closest_age = find_closest_age(age_to_find)
+    #print "DEBUG, closest age was", closest_age
     usecols = [0, closest_age.index.values[0] + 1] 
     names = ['wave', 'flam']
     df_bpass = pandas.read_table(bpassdir + filename, header=None, delim_whitespace=True, usecols=usecols, names=names)
     return(df_bpass)
     
-def wrap_load_1spectrum(Z, age, logU, style) :  #uses parameters rather than filenames
-    should_look_like = 'BPASSv2.1_imf135_100/spectra.z020.dat.gz'  # TESTING NOW
+def wrap_load_1spectrum(Z, age, style, IMFdir='BPASSv2.1_imf135_100/') :  #uses parameters rather than filenames
+    # filename should look like 'BPASSv2.1_imf135_100/spectra.z020.dat.gz'
     if 'binary' in style   : fileroot = 'spectra-bin'
     elif 'single' in style : fileroot = 'spectra'
     else : raise Exception("Misparsed style")
-    filename = default_filenames()[0] + fileroot + '.z' + Z + '.dat.gz'
-    print "DEBUGGING, filename", filename, "should look like", should_look_like
+    filename = IMFdir + fileroot + '.z' + Z + '.dat.gz'
+    print "    DEBUGGING filename in bpass.py, filename", filename
     df_bpass2 = load_1spectrum(filename, age)
     return(df_bpass2)  # call as wrap_load_1spectrum('020', 1E6, -2.0, "BPASS_single")
