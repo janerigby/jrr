e467ab44042a21a1c4c5c46fee3f1fdd1a75c35f
janerigby <jane.rigby@gmail.com>
2018-07-03 11:14:58 -0400

    Want to switch between BPASS v2.1, v2.2

--- bpass.py
+++ bpass.py
@@ -1,13 +1,15 @@
 #  Simple routines to read in BPASS2 spectra into pandas dataframes.  jrigby, Apr 2018
 import pandas
 
-def model_dir() : # Set this to where your models are
-    return('/Volumes/Apps_and_Docs/JRR_Utils/BPASS_v2.1/')
+def model_dir(ver='2.1') : # Set this to where your models are
+    if    ver=='2.1' :   return('/Volumes/Apps_and_Docs/JRR_Utils/BPASS_v2.1/')
+    elif  ver=='2.2' :   return('/Volumes/Apps_and_Docs/JRR_Utils/BPASS_v2.2/')
 
-def default_filenames() : # Default directory, model rootnames.  Default models, also what John wants for consistency w S99.
-    return ('BPASSv2.1_imf135_100/', 'BPASSv2p1_imf135_100_burst')  # dir, model_rootname
+def default_filenames(ver='2.1') : # Default directory, model rootnames.  Default models, also what John wants for consistency w S99.
+    if    ver=='2.1' :   return ('BPASSv2.1_imf135_100/',     'BPASSv2p1_imf135_100_burst')  # dir, model_rootname
+    elif  ver=='2.2' :   return ('BPASSv2.2_bin-imf135_100/', 'BPASSv2p2_imf135_100_burst')  # NOT TESTED YET
 
-def ages_setup() :  # Create a pandas dataframe of ages and column names ('coln'), as in BPASS v2.1 manual
+def ages_setup() :  # Create a pandas dataframe of ages and column names ('coln'), as in BPASS v2.1 manual.  Same for v2.2
     df_age = pandas.DataFrame([ 10**(6+0.1*(n-2)) for n in range(2,52+1)], columns=('age',))
     colnames = pandas.Series(["col"+str(n) for n in range(2,52+1)])
     df_age['colname'] = colnames
@@ -18,16 +20,16 @@ def find_closest_age(age_to_find):
     closest_age = df_age.iloc[(df_age['age']- age_to_find).abs().argsort()[:1]]
     return(closest_age) # Returns the row of df_age thats closest in age.  
 
-def load_spectra(filename) :  # Loads spectra for all ages.  1st col is wavelength ('wave'), in Angstroms
+def load_spectra(filename, ver='2.1') :  # Loads spectra for all ages.  1st col is wavelength ('wave'), in Angstroms
     # Uses filename, which should look like 'BPASSv2.1_imf135_100/spectra.z020.dat.gz'
-    bpassdir = model_dir()
+    bpassdir = model_dir(ver=ver)
     df_age = ages_setup()
     df_bpass = pandas.read_table(bpassdir + filename, header=None, delim_whitespace=True, names=['wave'] + df_age['colname'].tolist())
     return(df_bpass) 
 
-def load_1spectrum(filename, age_to_find) : #Streamline version of above, just loads 1 age (units=yr)  Faster.
+def load_1spectrum(filename, age_to_find, ver='2.1') : #Streamline version of above, just loads 1 age (units=yr)  Faster.
     # Uses filename, which should look like 'BPASSv2.1_imf135_100/spectra.z020.dat.gz'
-    bpassdir = model_dir()
+    bpassdir = model_dir(ver=ver)
     closest_age = find_closest_age(age_to_find)
     #print "DEBUG, closest age was", closest_age
     usecols = [0, closest_age.index.values[0] + 1] 
@@ -35,10 +37,13 @@ def load_1spectrum(filename, age_to_find) : #Streamline version of above, just l
     df_bpass = pandas.read_table(bpassdir + filename, header=None, delim_whitespace=True, usecols=usecols, names=names)
     return(df_bpass)
     
-def wrap_load_1spectrum(Z, age, style, IMFdir='BPASSv2.1_imf135_100/') :  #uses parameters rather than filenames. Units of age are years.
+def wrap_load_1spectrum(Z, age, style, ver='2.1') :  #uses parameters rather than filenames. Units of age are years.
     if 'binary' in style   : fileroot = 'spectra-bin'
-    elif 'single' in style : fileroot = 'spectra'
+    elif 'single' in style :
+        if      ver=='2.1' : fileroot = 'spectra'
+        elif if ver=='2.2' : fileroot = 'spectra-sin'
     else : raise Exception("Misparsed style")
+    IMFdir = default_filenames(ver=ver)[0]
     filename = IMFdir + fileroot + '.z' + Z + '.dat.gz'
     #print "    DEBUGGING filename in bpass.py, filename", filename
     df_bpass2 = load_1spectrum(filename, age)
