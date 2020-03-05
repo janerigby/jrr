14ab5f65b79a3eee64e6e96fb0044309a0d43a5e
janerigby <jane.rigby@gmail.com>
2020-03-05 13:23:45 -0500

    Updated link to John Chisholms Starburst99 fits

--- mage.py
+++ mage.py
@@ -277,8 +277,10 @@ def open_many_spectra(mage_mode, which_list="wcont", labels=(), verbose=True, zc
     
 def get_S99_path(MWdr=True) :  # Get path for S99
     (spec_path, line_path) = getpath("released")
-    if MWdr :      S99path = spec_path + "../Contrib/S99/MW_Corrected/"
-    else    :      S99path = spec_path + "../Contrib/S99/New_right_flam/"
+#    if MWdr :      S99path = spec_path + "../Contrib/S99/MW_Corrected/"
+#    else    :      S99path = spec_path + "../Contrib/S99/New_right_flam/"
+    if MWdr :      S99path = spec_path + "../Contrib/S99/Chisholm19/ApJ/Megasaura/"  #3/2020, updating to published S99 fits
+    else    :      S99path = spec_path + "../Contrib/S99/Obsolete/New_right_flam/"
     return(S99path)
     
 def getfullname_S99_spectrum(rootname, MWdr=True) : # Check whether a S99 spectrum exists
