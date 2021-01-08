c063aa6cec49357576a288101f649ee74d3577ac
janerigby <jane.rigby@gmail.com>
2021-01-08 15:21:20 -0500

    Moved all the lensing legwork to jrr/lens.py.  Made it work for RCS0327 too.  Made a deflection_norm parameter so that differently-normalized deflection maps can be read in.

--- lens.py
+++ lens.py
@@ -1,5 +1,8 @@
 from astropy import constants as const
+from astropy.coordinates import SkyCoord
+from astropy import units as u
 import numpy as np
+from jrr import util
 
 # Basic functions to work with Keren's lens models. 
 
@@ -22,3 +25,49 @@ def critical_density(cosmo, z_lens, z_src) :
     (D_L, D_S, D_LS) = lensing_distances(cosmo, z_lens, z_src)
     sigma_critical =  const.c **2  / (4. * np.pi * const.G) * D_S / (D_L * D_LS)
     return(sigma_critical)
+
+def make_lensed_regions_file(df_in, datadir, prefix, racol, deccol, color='green', suffix='reg') :  # Helper function stays in this file
+    # Helper function to make a ds9 regions file from a dataframe containing RA, DEC, ID of arcs
+    df_reg = df_in.copy(deep=True)
+    df_reg['text'] = 'text={' + df_reg['ID'].astype('str') + ' ' + suffix  + '}'
+    df_reg['color'] = 'color=' + color
+    df_reg['radius'] = '1.5'
+    util.make_ds9_regions_file(datadir + prefix + suffix + '.reg', df_reg, racol=racol, deccol=deccol)
+    return(df_reg)
+
+def compute_sourceplane_positions(arcs_df, prefix,z_lens, datadir, deflection_map, cosmo, deflection_norm=1, debug=False) :
+    # Compute source plane positions, given a dataframe of image plane positions and redshifts, and deflection maps in x,y
+    # see test_lensing_deflections.py for a worked example for A2744 and RCS0327
+    # def of deflection_norm:  The normalization for the deflection maps, which are scaled to some d_LS/d_S.  They scale linearly w d_LS/d_S.
+    prev_imgpos = SkyCoord(0.0, 0.0, unit='deg') # initalize
+    prev_srcpos = SkyCoord(0.0, 0.0, unit='deg') # initalize
+    print("Coordinates                                 distance from previous")
+    print("The distances for the first image in each family are meaningless")
+    print("ID   image plane    src plane               d_img    d_src")
+    df_imgplane_reg =  make_lensed_regions_file(arcs_df, datadir, prefix, 'RA', 'DEC', color='green', suffix='img') 
+    src_pos_list = []
+    for row in arcs_df.itertuples() :
+        img_pos = SkyCoord(row.RA, row.DEC, unit='deg')
+        z_src = row.zz
+        thisdeflect = {}
+        for key in deflection_map : #Loop through the X and Y deflection maps
+            defscale = scale_lensing_deflection(cosmo, z_lens, z_src) / deflection_norm
+            imval, xy = util.imval_at_coords(datadir + deflection_map[key], img_pos)
+            thisdeflect[key] = imval*defscale
+            if debug:
+                print("  ", row.ID, "  Deflection in", key, "at", xy, "(stupid numpy yx index):", np.round(imval, 2), end='  ')
+                print("scale by", np.round(defscale,3), "to get ", np.round(thisdeflect[key],2))
+        src_pos =  util.offset_coords(img_pos, delta_RA=thisdeflect['x'] *1, delta_DEC=thisdeflect['y']*-1) # minus sign here, per lensing eqn?
+        src_pos_list.append(util.print_skycoords_decimal(src_pos)) # save this to list, then put it in the data frame after the loop.
+        print(row.ID, util.print_skycoords_decimal(img_pos), util.print_skycoords_decimal(src_pos), end=' ')
+        print(prev_imgpos.separation(img_pos).to_string(unit=u.arcsec, precision=2),  prev_srcpos.separation(src_pos).to_string(unit=u.arcsec, precision=2))
+        prev_imgpos = img_pos
+        prev_srcpos = src_pos
+
+    # Store the source-plane positions in the dataframe.
+    arcs_df['temp'] = src_pos_list
+    arcs_df[['RA_srcplane', 'DEC_srcplane']] = arcs_df['temp'].str.split(expand=True).astype('float')
+    arcs_df.drop(['temp',],  inplace=True, axis=1)
+    arcs_df.to_csv(prefix + '_wsrccoords.csv')
+    df_srcplane_reg =  make_lensed_regions_file(arcs_df, datadir, prefix, 'RA_srcplane', 'DEC_srcplane', color='red', suffix='src') 
+    return(arcs_df)
